7176f48cfb35e8197e67d2c0b8c4a6ace766c5f1
Nico <menciere@amazon.com>
2024-02-20 23:07:53 -0500

    Updtate for bfan

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,20 +1,22 @@
 import { Handler } from 'aws-lambda';
-import { Bedrock } from 'langchain/llms/bedrock';
-import { BaseCallbackHandler } from 'langchain/callbacks';
-import { BufferMemory } from 'langchain/memory';
-import { ConversationChain } from 'langchain/chains';
-import { DynamoDBChatMessageHistory } from '@langchain/community/stores/message/dynamodb';
 import { decode, verify } from 'jsonwebtoken';
 import { promisify, TextEncoder } from 'util';
 import { ApiGatewayManagementApiClient, PostToConnectionCommand } from '@aws-sdk/client-apigatewaymanagementapi';
 import jwksRsa from 'jwks-rsa';
+import { BedrockAgentRuntimeClient, RetrieveAndGenerateCommand } from '@aws-sdk/client-bedrock-agent-runtime';
+import crypto from 'crypto';
+import { DynamoDB } from 'aws-sdk';
+
+const dynamodb = new DynamoDB();
 
 const textEncoder = new TextEncoder();
 
 const JWKS_URI = process.env.JWKS_URI ? process.env.JWKS_URI : '';
 const API_GW_ENDPOINT = process.env.API_GW_ENDPOINT;
-const ISSUER = process.env.ISSUER; 
+const ISSUER = process.env.ISSUER;
 const AUDIENCE = process.env.AUDIENCE ? process.env.AUDIENCE : '';
+
+const bedrock = new BedrockAgentRuntimeClient({ region: process.env.AWS_REGION });
 
 const apiGwManApiClient = new ApiGatewayManagementApiClient({
   region: process.env.AWS_REGION,
@@ -24,7 +26,7 @@ const apiGwManApiClient = new ApiGatewayManagementApiClient({
 const client = jwksRsa({
   cache: true,
   rateLimit: true,
-  jwksUri: JWKS_URI
+  jwksUri: JWKS_URI,
 });
 
 export const getSigningKey = promisify(client.getSigningKey);
@@ -82,26 +84,105 @@ async function authorize(token: string): Promise<boolean> {
   return false;
 }
 
+async function processPrompt(prompt: string, connectionId: string) {
+  console.log('Calling Bedrock');
+
+  let dynamo_params = {
+    TableName: 'bedrock_sessions',
+    KeyConditionExpression: 'connectionid = :connectionid',
+    ExpressionAttributeValues: {
+      ':connectionid': { S: connectionId },
+    },
+  };
+
+  let bedrock_sessionid = null;
+  dynamodb.query(dynamo_params, (err, data) => {
+    if (err) {
+      console.log('Error', err);
+    } else {
+      console.log('Query succeeded.');
+      if (data && data.Items && data.Items.length > 0) {
+        data.Items.forEach(function (item) {
+          console.log('Item:', item);
+          bedrock_sessionid = item.bedrock_sessionid;
+        });
+      }
+    }
+  });
+
+  var params = {
+    input: {
+      text: prompt,
+    },
+    retrieveAndGenerateConfiguration: {
+      type: 'KNOWLEDGE_BASE' /* required */,
+      knowledgeBaseConfiguration: {
+        knowledgeBaseId: 'JD6EOVOZMZ' /* required */,
+        modelArn:
+          'arn:aws:bedrock:' + process.env.AWS_REGION + '::foundation-model/anthropic.claude-instant-v1' /* required */,
+      },
+    },
+  };
+
+  if (bedrock_sessionid) {
+    params['sessionId'] = bedrock_sessionid;
+  }
+
+  const command = new RetrieveAndGenerateCommand(params);
+  const response = await bedrock.send(command);
+
+  console.log('Bedrock response:');
+  console.log(response);
+
+  await apiGwManApiClient.send(
+    new PostToConnectionCommand({
+      ConnectionId: connectionId,
+      Data: textEncoder.encode(response['output']['text']),
+    }),
+  );
+
+  await apiGwManApiClient.send(
+    new PostToConnectionCommand({
+      ConnectionId: connectionId,
+      Data: textEncoder.encode('End'),
+    }),
+  );
+
+  dynamo_params = {
+    TableName: 'bedrock_sessions',
+    Item: {
+      connectionid: {
+        S: connectionId,
+      },
+      bedrock_sessionid: {
+        S: response.sessionId,
+      },
+      ttl: {
+        N: (Date.now() + 1000 * 60 * 60 * 24 * 7).toString(),
+      },
+    },
+  };
+
+  await dynamodb.putItem(dynamo_params).promise();
+}
+
 export const handler: Handler = async (event: any, context: any) => {
   console.log('EVENT: \n' + JSON.stringify(event, null, 2));
   connectionId = event.requestContext.connectionId;
   const routeKey = event.requestContext.routeKey;
 
-  const memory = new BufferMemory({
-    chatHistory: new DynamoDBChatMessageHistory({
-      tableName: `conversationhistory`,
-      partitionKey: 'id',
-      sessionId: connectionId,
-    }),
-  });
+  console.log(`Connection id ${connectionId}`);
+  console.log(`Route key ${routeKey}`);
 
   switch (routeKey) {
     case '$connect': {
       console.log('$connect');
+      prompt = 'Hello!';
       break;
     }
     case '$disconnect': {
       console.log('$disconnect');
+      prompt = 'Bye!';
       break;
     }
     case 'ask': {
@@ -115,6 +196,8 @@ export const handler: Handler = async (event: any, context: any) => {
       } else {
         console.log('Authorized');
         prompt = requestData.data;
+
+        await processPrompt(prompt, connectionId);
       }
       break;
     }
@@ -123,44 +206,6 @@ export const handler: Handler = async (event: any, context: any) => {
       break;
     }
   }
-
-  console.log(`Connection id ${connectionId}`);
-  console.log(`Route key ${routeKey}`);
-
-  const callbackHandler = BaseCallbackHandler.fromMethods({
-    async handleLLMNewToken(token: string) {
-      console.log('token', { token });
-
-      const postToConnectionCommandResp = await apiGwManApiClient.send(
-        new PostToConnectionCommand({
-          ConnectionId: connectionId,
-          Data: textEncoder.encode(token),
-        }),
-      );
-      console.log(`postToConnectionCommand resp => ${JSON.stringify(postToConnectionCommandResp)}`);
-    },
-    async handleLLMEnd() {
-      const postToConnectionCommandResp = await apiGwManApiClient.send(
-        new PostToConnectionCommand({
-          ConnectionId: connectionId,
-          Data: textEncoder.encode('End'),
-        }),
-      );
-      console.log(`postToConnectionCommand resp => ${JSON.stringify(postToConnectionCommandResp)}`);
-    },
-  });
-
-  const model = new Bedrock({
-    model: 'anthropic.claude-v2',
-    region: 'us-east-1',
-    streaming: true,
-    callbacks: [callbackHandler],
-    modelKwargs: { max_tokens_to_sample: 4000, temperature: 0.1 },
-  });
-
-  const chain = new ConversationChain({ llm: model, memory });
-  const res = await chain.predict({input: `\n\nHuman: ${prompt} \n\nAssistant:`});
-  console.log(res);
 
   return {
     statusCode: 200,
--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -9,9 +9,9 @@ import lens from './assets/lens.png';
 import spinner from './assets/spinner.gif';
 
 const REGION = 'us-east-1';
-const USER_POOL_ID = '';
-const USER_POOL_WEB_CLIENT_ID = '';
-const API_ENDPOINT = '';
+const USER_POOL_ID = 'us-east-1_POyd1xvtJ';
+const USER_POOL_WEB_CLIENT_ID = '6rq1t7ke88tmf256ujd103b8s2';
+const API_ENDPOINT = 'wss://z830t9oz24.execute-api.us-east-1.amazonaws.com/dev/';
 
 Amplify.configure({
   Auth: {
@@ -23,48 +23,99 @@ Amplify.configure({
 });
 
 function App() {
-  const { lastJsonMessage, sendJsonMessage, readyState } = useWebSocket(
-    API_ENDPOINT,
-    {
-      onOpen: () => {
-        console.log('WebSocket connection established.');
-      },
-      onMessage(event) {
-        if (event.data != 'End') { // Indicates no more tokens
-          if (!event.data.includes('Endpoint request timed out')) { // Exclude APIGateway timeout message
-            setAnswer(answer + event.data);
-          }
-        } else {
-          setLoading(false);
-        }
-      },
-      share: true,
-      filter: () => false,
-      retryOnError: true,
-      shouldReconnect: () => true,
+  const [prompt, setPrompt] = useState(undefined);
+  const [loading, setLoading] = useState(false);
+  const [conversation, setConversation] = useState([]);
+
+  const { lastJsonMessage, sendJsonMessage, readyState } = useWebSocket(API_ENDPOINT, {
+    onOpen: () => {
+      console.log('WebSocket connection established.');
     },
-  );
+    onMessage(event) {
+      if (event.data != 'End') {
+        // Indicates no more tokens
+        if (!event.data.includes('Endpoint request timed out')) {
+          // Exclude APIGateway timeout message
+          // setAnswer(answer + event.data);
+
+          setConversation([
+            ...conversation,
+            {
+              text: event.data,
+              type: 'answer',
+            },
+          ]);
+        }
+      } else {
+        setLoading(false);
+      }
+    },
+    share: true,
+    filter: () => false,
+    retryOnError: true,
+    shouldReconnect: () => true,
+  });
 
   const sendPrompt = async (event) => {
     if (event.key !== 'Enter') {
       return;
     }
-    setAnswer('');
     setLoading(true);
     sendJsonMessage({
       action: 'ask',
       data: prompt,
       token: (await Auth.currentSession()).getIdToken().getJwtToken(),
     });
-  };
+    setConversation([
+      ...conversation,
+      {
+        text: prompt,
+        type: 'question',
+      },
+    ]);
 
-  const [prompt, updatePrompt] = useState(undefined);
-  const [loading, setLoading] = useState(false);
-  const [answer, setAnswer] = useState('');
+    setPrompt('');
+  };
 
   return (
     <div className="app">
-      <div className="app-container">
+      <div id="chatborder">
+        <div id="chatbox">
+          {conversation.reverse().map((item, index) => (
+            <p key={`${'chatmsg' + index}`} className={`${item.type == 'question' ? 'question' : 'answer'}`}>
+              {item.text}
+            </p>
+          ))}
+        </div>
+
+        <div className="chatwrapper">
+          <input
+            type="text"
+            name="prompt"
+            id="prompt"
+            disabled={loading}
+            className="chatinput"
+            style={{
+              backgroundImage: loading ? `url(${spinner})` : `url(${lens})`,
+            }}
+            placeholder="Hi there! Type here to talk to me."
+            onChange={(e) => setPrompt(e.target.value)}
+            onKeyDown={(e) => sendPrompt(e)}
+          />
+        </div>
+      </div>
+
+      {/* <div className="app-container">
+        <div>
+          {conversation.reverse().map((item, index) => (
+            <div>{item.text}</div>
+          ))}
+        </div>
+
+        {/* <div className="answer">
+          <ReactMarkdown>{answer}</ReactMarkdown>
+        </div>
+
         <div className="spotlight__wrapper">
           <input
             type="text"
@@ -72,20 +123,17 @@ function App() {
             placeholder="Ask me anything..."
             style={{
               backgroundImage: `url(${lens})`,
+              width: '100%',
             }}
             disabled={loading}
             style={{
               backgroundImage: loading ? `url(${spinner})` : `url(${lens})`,
             }}
-            onChange={(e) => updatePrompt(e.target.value)}
+            onChange={(e) => setPrompt(e.target.value)}
             onKeyDown={(e) => sendPrompt(e)}
           />
         </div>
-
-        <div className="answer">
-          <ReactMarkdown>{answer}</ReactMarkdown>
-        </div>
-      </div>
+      </div> */}
     </div>
   );
 }
